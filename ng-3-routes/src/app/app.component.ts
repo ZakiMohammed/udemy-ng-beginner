ca5f1c2977d5ed7d9df93bad2e79f797219fa71f
ZakiMohammed <shaihzaki67@gmail.com>
2025-10-11 00:14:38 +0300

    NG3: Implemented app routing

--- ng-3-routes/src/app/app.component.ts
+++ ng-3-routes/src/app/app.component.ts
@@ -2,13 +2,11 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
-import { FormComponent } from './components/form/form.component';
-import { ListComponent } from './components/list/list.component';
-import { EmptyComponent } from './components/empty/empty.component';
+import { NavbarComponent } from "./components/navbar/navbar.component";
 
 @Component({
   selector: 'app-root',
-  imports: [HeaderComponent, FooterComponent, FormComponent, ListComponent, EmptyComponent],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent, NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
