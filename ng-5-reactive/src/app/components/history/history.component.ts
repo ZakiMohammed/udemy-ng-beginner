740ac78d5301b27dc1937c270d77623d11faa946
ZakiMohammed <shaihzaki67@gmail.com>
2025-11-21 19:17:48 +0530

    NG5: Added colors for type and updated text

--- ng-5-reactive/src/app/components/history/history.component.ts
+++ ng-5-reactive/src/app/components/history/history.component.ts
@@ -2,10 +2,11 @@ import { Component, effect, inject, OnInit } from '@angular/core';
 import { NoteService } from '../../services/note.service';
 import { NoteAction, storageKeys } from '../../models/note.model';
 import { LocalStorageService } from '../../services/local-storage.service';
+import { UpperCasePipe, NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-history',
-  imports: [],
+  imports: [UpperCasePipe, NgClass],
   templateUrl: './history.component.html',
   styleUrl: './history.component.scss',
 })
@@ -40,8 +41,14 @@ export class HistoryComponent implements OnInit {
     this.localService.clear();
   }
 
-  getActionText(action: NoteAction) {
-    const payload = action.payload ? ` - ${action.payload.id}` : '';
-    return `${action.type}${payload}`;
+  getTypeClass(action: NoteAction) {
+    switch (action.type) {
+      case 'Add':
+        return 'text-bg-success';
+      case 'Remove':
+        return 'text-bg-danger';
+      default:
+        return 'text-bg-secondary';
+    }
   }
 }
